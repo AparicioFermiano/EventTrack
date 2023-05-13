c253ec205c4deef8b90924d07c564a5638e08516
AparicioFermiano <aparicio.empresarial@gmail.com>
2023-05-13 19:34:53 -0300

    Login pronto, precisa arrumar o front para exibir a mensagem

--- src/app/login-usuario/login-usuario.component.ts
+++ src/app/login-usuario/login-usuario.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-usuario',
@@ -7,20 +10,51 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./login-usuario.component.css']
 })
 export class LoginUsuarioComponent {
+  login: FormGroup = new FormGroup({});
   email: string;
   password: string;
   message: string;
 
-  constructor(private http: HttpClient) {
+  constructor(
+      private http: HttpClient, 
+      private router: Router,
+      private formBuilder: FormBuilder) {
     this.email = '';
     this.password = '';
     this.message = '';
+    this.login = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email, Validators.minLength(1)]],
+      password: ['',[Validators.required, Validators.minLength(1)]]
+    });
   }
 
-  async submitForm() {
-    const url = 'http://localhost:3000/login';
-    const body = { email: this.email, password: this.password };
-    const response = await this.http.post(url, body).toPromise();
-    console.log(response)
+  submitForm() {
+    console.log(this.login.valid)
+    if (this.login.valid) {
+      const body = { email: this.login.get('email')?.value, password: this.login.get('password')?.value };
+      return this.http.post('http://localhost:3000/login/validar_login', body).subscribe(
+        (res: any) => {
+          if(res.status == 200){
+            this.message = 'Login com sucesso';
+            this.router.navigate(['/']);
+          } else {
+            this.message = 'E-mail ou senha inválidos';
+          }
+        },
+        (err) => {
+          this.message = JSON.stringify(err);
+        }
+      );
+    } else {
+      if (this.login.get('email')?.errors?.['required']) {
+        this.message = 'O email é obrigatório';
+      } else if (this.login.get('email')?.errors?.['email']) {
+        this.message = 'O email não está no formato correto';
+      } else if (this.login.get('password')?.errors?.['required']) {
+        this.message = 'A senha é obrigatória';
+      }
+    }
+
+    return false;
   }
 }
